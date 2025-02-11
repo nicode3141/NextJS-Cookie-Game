aa9c0d4589933959c0cb74bb65071f1a95d6caa5
nicode3141 <nicolas.d.pfeifer@gmail.com>
2025-02-11 21:24:58 +0100

    database fuctionality with drizzle and neon

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Simple Cookie Clicker clone with authentication
 1. [ ] Setup Pages (Layout & Design)
 2. [ ] Setup NextAuth
 3. [ ] Protect Route / Login Page
-4. [ ] Database Setup (Drizzle Adapter)
+4. [x] Database Setup (Drizzle Adapter)
 5. [ ] API Support
 6. [ ] Manage Users
 
--- src/pages/cookie.tsx
+++ src/pages/cookie.tsx
@@ -2,7 +2,8 @@
 
 import { motion } from "framer-motion"
 import { Montserrat, Inter } from "next/font/google";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { getCookieCount } from "./api/cookies";
 
 const montserratFont = Montserrat({
     weight: ['300', '400', '500', '600', '700', '800', '900'],
@@ -16,12 +17,45 @@ const interFont = Inter({
     subsets: ["latin"],
 });
 
-export default function Cookie(){
+
+
+function Cookie(){
     const [cookieCount, setCookieCount] = useState<number>(0);
     
     function handleClick(){
         setCookieCount((prev) => prev + 1);
     }
+
+    async function fetchCookies(){
+        const cookieCount = await getCookieCount()
+        return cookieCount;
+    }
+
+    useEffect(() => {
+        fetch("/api/cookies")
+            .then((res) => res.json())
+            .then((data) => setCookieCount(data.cookies));
+
+        console.log(fetchCookies());
+    }, []);
+
+    
+    useEffect(() => {
+        if(cookieCount % 10 == 0){
+            var userParams = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    cookies: cookieCount,
+                    userID: "test"
+                })
+              };
+
+            fetch("/api/cookies", userParams)
+        }
+    }, [cookieCount, ]);
 
     return(
         <div className={`flex items-center justify-center p-5 flex-col bg-slate-100 h-screen ${interFont.className}`}>
@@ -38,3 +72,5 @@ export default function Cookie(){
         </div>
     )
 }
+
+export default Cookie;
